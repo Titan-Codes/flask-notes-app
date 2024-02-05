bdce1f5718d23052aaa1043073a842d67c6a6211
Titan-Codes <vedansh1sharma2005@gmail.com>
2024-02-05 20:31:34 +0530

    signup and login page.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login")
 def login():
-    return "Login page"
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
-    return "Logout page"
+    return "Logged out"
 
 @auth.route("/signup")
 def signup():
-    return "Sign up page"
+    return render_template("signup.html")
