ef6b527a83a1dd47bce37236b428d7f3af187494
Titan-Codes <vedansh1sharma2005@gmail.com>
2024-02-11 17:43:08 +0530

    fixed login page

--- website/auth.py
+++ website/auth.py
@@ -8,12 +8,6 @@ auth = Blueprint('auth', __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html")
-
-@auth.route("/logout")
-def logout():
     if request.method == 'POST':
         email = request.form.get("email")
         password = request.form.get("password")
@@ -22,12 +16,18 @@ def logout():
         if user:
             if user.password == password:
                 flash("Logged in successfully!", category="success")
+                return redirect(url_for("views.home"))
             else:
                 flash("Incorrect password, try again.", category="error")
         else:
             flash("Email does not exist.", category="error")
             # return redirect(url_for("views.signup"))
-    return redirect(url_for("views.home"))
+    # return redirect(url_for("views.home"))
+    return render_template("login.html", boolean=True)
+
+@auth.route("/logout")
+def logout():
+    return "Logged out"
 
 @auth.route("/signup", methods=['GET', 'POST'])
 def signup():
