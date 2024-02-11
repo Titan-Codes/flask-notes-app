f4526eea11b469ae1698f9ce344a178e76058531
Titan-Codes <vedansh1sharma2005@gmail.com>
2024-02-12 01:16:54 +0530

    added delete note feature

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.sql import func
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(100000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
